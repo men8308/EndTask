c1ad59a5b5b10e6ee7dccfb5adb012487f9c8c6c
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-03-11 04:12:48 +0200

    Добавил код решения задачи

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+String[] array1 = new string[5] { "Hi", "Hello", "123", "46584", "?" };
+String[] array2 = new string[array1.Length];
+
+SecondArrayFill(array1, array2);
+PrintArray(array2);
+
+void SecondArrayFill (string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length<=3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    } 
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
